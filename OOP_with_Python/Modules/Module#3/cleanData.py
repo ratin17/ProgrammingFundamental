0686b133f98230589859cbb00bb8f50a04c2e732
ratin17 <ratin.cse@gmail.com>
2022-10-09 11:13:43 +0600

    cleaning data-py

---
+++ OOP_with_Python/Modules/Module#3/cleanData.py
@@ -0,0 +1,14 @@
+data="jhsdgvf'fd[sbdfghe[]f;'\n\n.,p[gaterwq  JK>>MHG^&*_)_++_)*$!@#FCBNMN><>><?MNBVXZAWETUIPPOKNBVDSSXCV  nbbvnmxcvipougfbhvfhbdf  hjhrbvfdsdew/,;[-=087125284vnxmzaavnmiyrtrhf"
+
+cleanData=""
+
+for char in data:
+    if char.lower()=='a' or char.lower()=='e' or char.lower()=='i' or char.lower()=='o' or char.lower()=='u':
+        if len(cleanData)!=0:
+            cleanData+=f" {char}"
+        else:
+            cleanData+=char
+
+print(cleanData)
+
+
