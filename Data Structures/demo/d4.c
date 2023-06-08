5d10a69a37e79efe7599b5a32d63647a93a19d69
Mehrab Rabbi Ratin <ratin.cse@gmail.com>
2023-06-08 18:13:10 +0600

    insertion added in demo

--- Data Structures/demo/d4.c
+++ Data Structures/demo/d4.c
@@ -1,24 +1,48 @@
 #include<stdio.h>
 #include<string.h>
-int main()
-{
-    int s[100];
-    int i;
-    scanf("%s",r);
-    int sum=0;
-    int n=strlen(r);
-    printf("%d\n",n);
-    for(i=0;i<n;i++){
-        printf("%d ",i);
-        printf("%c ",s[i]);
-        int t=(int)s[i]-96; 
-        sum=sum+t;
-        printf("%d ",t);
+int main(){
+    int n,k;
+    scanf("%d %d",&n,&k);
+    long long int arr[n];
+
+    for(int i=0;i<n;i++){
+        scanf("%lld",&arr[i]);
     }
-    
-    // for(i=0;i<strlen(s);i++)
-    // {
-    //     printf("%d ",s[i]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+ 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    // for(int i=0;i<n-1;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(arr[i]<arr[j]){
+    //             long long int t=arr[i];
+    //             arr[i]=arr[j];
+    //             arr[j]=t;
+    //         }
+    //     }
     // }
+
+    for(int i=0;i<n;i++){
+        printf("%lld ",arr[i]);
+    }
+    printf("\n");
+
+
     return 0;
 }
+
+/*
+
+6 4
+6 9 0 -1 5 6
+
+*/
