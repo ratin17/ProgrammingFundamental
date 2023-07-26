2888945809e94905704b794967bb9db8b7951b78
Mehrab Rabbi Ratin <ratin.cse@gmail.com>
2023-07-26 11:28:53 +0600

    demo c++ code added

---
+++ Data Structures/demo/emni.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n ;
+cin>>n;
+for (int i = 2; i <= sqrt(n); i++){
+    if(n%i==0){
+        cout<<i<<" ";
+        if(n/i != i) cout<<n/i<<endl;
+    }
+}
+
+return 0;
+
+}
